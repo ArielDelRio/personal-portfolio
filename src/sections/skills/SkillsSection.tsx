77038c37067c9424f8d7c6f0694a94d7ac0c9e1f
ArieldRio <arieldelrioviamonte@gmail.com>
2021-08-12 17:24:03 -0400

    add svg logos and test skills boxes

--- src/sections/skills/SkillsSection.tsx
+++ src/sections/skills/SkillsSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Center, Heading } from "@chakra-ui/react";
+import { Box, Center, Heading, Image, transform } from "@chakra-ui/react";
+import javascript_logo from "../../images/skill_logos/javascript.svg";
 
 const SkillsSection = () => {
   return (
@@ -19,6 +20,83 @@ const SkillsSection = () => {
         >
           Skills
         </Heading>
+      </Center>
+      <Center>
+        <Box mt="32">
+          <Box
+            className="flip-card"
+            w="24"
+            h="24"
+            mb="10"
+            bg="transparent"
+            transform="rotate(45deg);"
+            cursor="pointer"
+            style={{ perspective: "1000px" }}
+          >
+            <Box
+              className="flip-card-inner"
+              position="relative"
+              w="100%"
+              h="100%"
+              transition="transform 0.8s"
+              _hover={{ transform: "rotateY(180deg)" }}
+              style={{ transformStyle: "preserve-3d" }}
+            >
+              <Box
+                className="flip-card-front"
+                position="absolute"
+                w="100%"
+                h="100%"
+                bg="#C4C4C4"
+                style={{ backfaceVisibility: "hidden" }}
+              >
+                <Center h="100%">
+                  <Image
+                    src={javascript_logo}
+                    alt="javascript logo"
+                    boxSize="14"
+                    transform="rotate(-45deg);"
+                  />
+                </Center>
+              </Box>
+              <Box
+                className="flip-card-back"
+                position="absolute"
+                w="100%"
+                h="100%"
+                bg="linear-gradient(271deg, #C4C4C4 -1.98%, rgba(196, 196, 196, 0) 96.46%);"
+                style={{ backfaceVisibility: "hidden" }}
+                transform="rotateY(180deg);"
+              >
+                back
+              </Box>
+            </Box>
+          </Box>
+          <Box
+            w="24"
+            h="24"
+            mb="10"
+            bg="#C4C4C4"
+            transform="rotate(45deg);"
+            cursor="pointer"
+          ></Box>
+          <Box
+            w="24"
+            h="24"
+            mb="10"
+            bg="#C4C4C4"
+            transform="rotate(45deg);"
+            cursor="pointer"
+          ></Box>
+          <Box
+            w="24"
+            h="24"
+            mb="10"
+            bg="#C4C4C4"
+            transform="rotate(45deg);"
+            cursor="pointer"
+          ></Box>
+        </Box>
       </Center>
     </Box>
   );
