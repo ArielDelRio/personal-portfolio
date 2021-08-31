5bddd07e0f78e10dddb12ab8b21f5faca4109ace
ArieldRio <arieldelrioviamonte@gmail.com>
2021-08-31 16:39:06 -0400

    Remove options in gatsby-plugin-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,23 +50,15 @@ module.exports = {
       },
     },
     "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: `gatsby-plugin-sharp`,
+      resolve: "gatsby-plugin-google-fonts",
       options: {
-        // // Defaults used for gatsbyImageData and StaticImage
-        // defaults: {},
-        // // Set to false to allow builds to continue on image errors
-        // failOnError: true,
-        // // deprecated options and their defaults:
-        // base64Width: 20,
-        // forceBase64Format: `png`, // valid formats: png,jpg,webp
-        // useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
-        // stripMetadata: true,
-        // defaultQuality: 50,
-        icon: "src/images/gatsby-icon.png",
+        fonts: [`Roboto`, `Dancing Script`],
+        display: "swap",
       },
     },
-    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-plugin-google-fonts",
       options: {
